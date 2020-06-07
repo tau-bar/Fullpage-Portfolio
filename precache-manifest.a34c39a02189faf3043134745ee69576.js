50b11a41efc50c443d1c448ec1eebedfea6156b4
Taufiq Abdul Rahman <tau-bar@users.noreply.github.com>
2020-06-08 01:24:17 +0800

    Updates

---
+++ precache-manifest.a34c39a02189faf3043134745ee69576.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274f5535fe2759b5cac93c212f123e15",
+    "url": "/fp-portfolio/index.html"
+  },
+  {
+    "revision": "65a4586842124961cad3",
+    "url": "/fp-portfolio/static/css/main.f7d6f1e4.chunk.css"
+  },
+  {
+    "revision": "a3957c92a3b722b501c0",
+    "url": "/fp-portfolio/static/js/2.172c06ec.chunk.js"
+  },
+  {
+    "revision": "e95b3ebaec68caad50c330e63af5be13",
+    "url": "/fp-portfolio/static/js/2.172c06ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65a4586842124961cad3",
+    "url": "/fp-portfolio/static/js/main.07f03ca2.chunk.js"
+  },
+  {
+    "revision": "48bc0855f37d93c6e4c1",
+    "url": "/fp-portfolio/static/js/runtime-main.20080668.js"
+  },
+  {
+    "revision": "e4c1c9e1ee2e572940dd34e1e008903e",
+    "url": "/fp-portfolio/static/media/trident.e4c1c9e1.gif"
+  },
+  {
+    "revision": "f71cd4c56e99f7443d84c56d3ed1cbfa",
+    "url": "/fp-portfolio/static/media/trident.f71cd4c5.png"
+  },
+  {
+    "revision": "ac0a0846ab32cd1b3752d90efcbf487d",
+    "url": "/fp-portfolio/static/media/zulibakes.ac0a0846.gif"
+  },
+  {
+    "revision": "fbbbc4562a5e0cf36107a1a9ad88413e",
+    "url": "/fp-portfolio/static/media/zulibakes.fbbbc456.png"
+  }
+]);
